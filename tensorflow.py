a3995afbb31c8a6803b6e8188541da72a2c9d2e0
wyuchi99 <wyuchi99@gmail.com>
2020-05-12 06:56:06 +0800

    123

---
+++ tensorflow.py
@@ -0,0 +1,7 @@
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file(
+    "iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
