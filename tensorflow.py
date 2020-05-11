a7d2314c7d897ecb5e26613c5701cd27f2e4fc1b
wyuchi99 <wyuchi99@gmail.com>
2020-05-12 06:59:32 +0800

    `12

--- tensorflow.py
+++ tensorflow.py
@@ -1,3 +1,9 @@
+import tensorflow as tf
+
+import pandas as pd
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+
 train_path = tf.keras.utils.get_file(
     "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
 test_path = tf.keras.utils.get_file(
